4022c53a6be55196171b6ebb9cafdcad7d93125e
ketmar@ketmar.no-ip.org <ketmar@ketmar.no-ip.org>
2021-02-20 07:34:21 +0000

    script: psim: cosmetix
    FossilOrigin-Name: 56fe9e5fd8afa25ec370c33858b3a63e8e7bf20612f6d03ae73716e5982613d3

--- source/psim/p_entity.cpp
+++ source/psim/p_entity.cpp
@@ -920,7 +920,7 @@ bool VEntity::CallStateChain (VEntity *Actor, VState *AState) {
     if (++RunAway > 512) {
       GCon->Logf(NAME_Warning, "entity '%s' state chain interrupted by WatchCat!", *Actor->GetClass()->GetFullName());
       GCon->Logf(NAME_Warning, "... state: '%s'", *S->Loc.toStringNoCol());
-      res = false; // watchcat break, oops
+      res = 0; // watchcat break, oops
       break;
     }
 
