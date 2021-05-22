ae6aada88b9bee4e82ed74741893c8bb2139b9fe
ketmar@ketmar.no-ip.org <ketmar@ketmar.no-ip.org>
2021-05-22 05:46:32 +0000

    shader: some experiments
    FossilOrigin-Name: 8a9a4a55cb6b246dada515e6de2724de1df90ee086a59e1bd56681796147731a

--- basev/common/glshaders/common/glow_vars.fs
+++ basev/common/glshaders/common/glow_vars.fs
@@ -17,5 +17,22 @@ vec4 calcGlow (vec4 light) {
   lt.g = clamp(light.g+GlowColorFloor.g*fh+GlowColorCeiling.g*ch, 0.0, 1.0);
   lt.b = clamp(light.b+GlowColorFloor.b*fh+GlowColorCeiling.b*ch, 0.0, 1.0);
   lt.a = light.a;
+
+#if 0
+  // this doesn't work right
+  float i = clamp(lt.r*0.2989+lt.g*0.5870+lt.b*0.1140, 0.0, 1.0);
+  if (i < 0.1) {
+#ifdef VAVOOM_REVERSE_Z
+    float z = 1.0/gl_FragCoord.w;
+#else
+    float z = gl_FragCoord.z/gl_FragCoord.w;
+#endif
+    if (z < 64.0) {
+      float n = (1.0-z/64.0)*0.5;
+      lt.rgb = clamp(lt.rgb+vec3(n, n, n), vec3(0.0), vec3(1.0));
+    }
+  }
+#endif
+
   return lt;
 }
