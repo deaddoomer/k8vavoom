b9bf8f610357f78f248d4f2ca382a34893521a42
ketmar@ketmar.no-ip.org <ketmar@ketmar.no-ip.org>
2021-05-24 19:45:51 +0000

    corelib: use "VAVOOM_MIMALLOC=0" envvar to disable mi-malloc (useful for valgrind and gdb)
    FossilOrigin-Name: 9b9194c2b2e8cde20eb0d279b4a707aca279921d6d6dff6d614e8d996a1656c7

--- libs/core/zone.cpp
+++ libs/core/zone.cpp
@@ -43,22 +43,77 @@ int zone_free_call_count = 0;
 
 
 #ifdef VAVOOM_USE_MIMALLOC
-static volatile bool zShuttingDown = false;
+static volatile uint32_t zShuttingDown = 0;
+//   0: not initialized
+// 666: initializing
+//   1: allowed
+//  69: disabled
+static volatile uint32_t zMimallocAllowed = 0u;
+#endif
+
+
+#ifdef VAVOOM_USE_MIMALLOC
+static bool xstrEquCI (const char *s0, const char *s1) noexcept {
+  if (!s0) return !s1;
+  if (!s1) return false;
+  while (*s0 && ((uint8_t)*s0) <= 32) ++s0;
+  while (*s1 && ((uint8_t)*s1) <= 32) ++s1;
+  while (*s0 && *s1) {
+    uint8_t c0 = (uint8_t)*s0++;
+    if (c0 >= 'A' && c0 <= 'Z') c0 = c0-'A'+'a';
+    uint8_t c1 = (uint8_t)*s1++;
+    if (c1 >= 'A' && c1 <= 'Z') c1 = c1-'A'+'a';
+    if (c0 != c1) return false;
+  }
+  while (*s0 && ((uint8_t)*s0) <= 32) ++s0;
+  while (*s1 && ((uint8_t)*s1) <= 32) ++s1;
+  return (!*s0 && !*s1);
+}
+
+
+static inline bool zIsMimallocAllowed () noexcept {
+#ifndef _WIN32
+  uint32_t v = 0u; // value to compare
+  if (__atomic_compare_exchange_n(&zMimallocAllowed, &v, 666u, false, __ATOMIC_SEQ_CST, __ATOMIC_SEQ_CST)) {
+    // `zMimallocAllowed` was zero (now 666u), need to init
+    v = 1u;
+    const char *ee = getenv("VAVOOM_MIMALLOC");
+    if (ee) {
+      if (xstrEquCI(ee, "0") ||
+          xstrEquCI(ee, "ona") ||
+          xstrEquCI(ee, "off") ||
+          xstrEquCI(ee, "no"))
+      {
+        fprintf(stderr, "ZONE WARNING: MIMALLOC DISABLED!\n");
+        v = 69u;
+      }
+    }
+    __atomic_store_n(&zMimallocAllowed, v, __ATOMIC_SEQ_CST);
+  } else {
+    // `zMimallocAllowed` is non-zero (now 666u), and copied to `v`
+    if (v == 666u) {
+      // currently initializing, wait
+      while (__atomic_load_n(&zMimallocAllowed, __ATOMIC_SEQ_CST) == 666u) {}
+    }
+  }
+  return (v == 1u);
+#else
+  return true;
+#endif
+}
 #endif
 
 
 static inline void ZManDeactivate () noexcept {
 #ifdef VAVOOM_USE_MIMALLOC
-  __atomic_store_n(&zShuttingDown, true, __ATOMIC_SEQ_CST);
+  __atomic_store_n(&zShuttingDown, 1u, __ATOMIC_SEQ_CST);
 #endif
 }
 
 
 static inline bool isZManActive () noexcept {
 #ifdef VAVOOM_USE_MIMALLOC
-  bool res = false;
-  __atomic_load(&zShuttingDown, &res, __ATOMIC_SEQ_CST);
-  return !res;
+  return (__atomic_load_n(&zShuttingDown, __ATOMIC_SEQ_CST) == 0u);
 #else
   return true;
 #endif
@@ -66,19 +121,19 @@ static inline bool isZManActive () noexcept {
 
 
 #ifdef VAVOOM_USE_MIMALLOC
-# define malloc_fn   ::mi_malloc
-# define realloc_fn  ::mi_realloc
-# define free_fn     ::mi_free
+# define malloc_fn(sz_)        (zIsMimallocAllowed() ? ::mi_malloc(sz_) : ::malloc(sz_))
+# define realloc_fn(ptr_,sz_)  (zIsMimallocAllowed() ? ::mi_realloc(ptr_,sz_) : ::realloc(ptr_,sz_))
+# define free_fn(ptr_)         (zIsMimallocAllowed() ? ::mi_free(ptr_) : ::free(ptr_))
 #else
-# define malloc_fn   ::malloc
-# define realloc_fn  ::realloc
-# define free_fn     ::free
+# define malloc_fn(sz_)        ::malloc(sz_)
+# define realloc_fn(ptr_,sz_)  ::realloc(ptr_,sz_)
+# define free_fn(ptr_)         ::free(ptr_)
 #endif
 
 
 const char *Z_GetAllocatorType () noexcept {
 #ifdef VAVOOM_USE_MIMALLOC
-  return "mi-malloc";
+  return (zIsMimallocAllowed() ? "mi-malloc" : "standard");
 #else
   return "standard";
 #endif
@@ -90,9 +145,10 @@ void *Z_Malloc (size_t size) noexcept {
 #ifdef VAVOOM_CORE_COUNT_ALLOCS
   ++zone_malloc_call_count;
 #endif
-  void *res = malloc_fn(size > 0 ? size : size+1);
+  size += !size;
+  void *res = malloc_fn(size);
   if (!res) Sys_Error("out of memory for %u bytes!", (unsigned int)size);
-  memset(res, 0, size+(size ? 0 : 1)); // just in case
+  memset(res, 0, size); // just in case
   return res;
 }
 
@@ -117,15 +173,18 @@ __attribute__((alloc_size(2))) void *Z_Realloc (void *ptr, size_t size) noexcept
 
 __attribute__((malloc)) __attribute__((alloc_size(1))) __attribute__((returns_nonnull))
 void *Z_Calloc (size_t size) noexcept {
+  size += !size;
 #if !defined(VAVOOM_USE_MIMALLOC)
-  void *res = ::calloc(1, (size > 0 ? size : 1));
+  void *res = ::calloc(1, size);
 #else
-  void *res = ::mi_zalloc(size > 0 ? size : 1);
+  void *res = (zIsMimallocAllowed() ? ::mi_zalloc(size) : ::calloc(1, size));
 #endif
   if (!res) Sys_Error("out of memory for %u bytes!", (unsigned int)size);
+/*
 #if !defined(VAVOOM_USE_MIMALLOC)
-  memset(res, 0, size+(size ? 0 : 1)); // just in case
+  memset(res, 0, size); // just in case
 #endif
+*/
   return res;
 }
 
