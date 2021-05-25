8574ad6514ca52b67b188d25fd239d0af1d045a0
ketmar@ketmar.no-ip.org <ketmar@ketmar.no-ip.org>
2021-05-25 20:31:57 +0000

    shaders: cosmetix
    FossilOrigin-Name: 77aa9e0968d370b6e5970679a0fed1c5409d2c465249087e589e3c04fbdcb3e8

--- basev/common/glshaders/tonemap/tonemap_palette_64.fs
+++ basev/common/glshaders/tonemap/tonemap_palette_64.fs
@@ -7,7 +7,6 @@ uniform sampler2D TexPalLUT;
 
 
 vec3 Tonemap (vec3 color) {
-#if 1
   ivec3 c = ivec3(clamp(color.rgb, vec3(0.0), vec3(1.0))*63.0+0.5);
   int index = int((c.r*64+c.g)*64+c.b);
   float tx = int(mod(index, 512))+0.5;
@@ -15,11 +14,6 @@ vec3 Tonemap (vec3 color) {
   float ty = int(index/512)+0.5;
   ty /= 512.0;
   return texture2D(TexPalLUT, vec2(tx, ty)).rgb;
-  //return texture2D(TexPalLUT, vec2(0.0, 0.0)).rgb;
-#else
-  float i = clamp(color.r*0.2989+color.g*0.5870+color.b*0.1140, 0.0, 1.0);
-  return vec3(i, i, i);
-#endif
 }
 
 
