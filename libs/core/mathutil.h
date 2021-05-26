ae0a5036fd35d39425e7161f2662b6a0e95667f0
ketmar@ketmar.no-ip.org <ketmar@ketmar.no-ip.org>
2021-05-26 04:22:44 +0000

    corelib: some function attrs
    FossilOrigin-Name: 153aef3fe08ecc86d5533cb192b437b7500377c00bf308d97972c6db890f56be

--- libs/core/mathutil.h
+++ libs/core/mathutil.h
@@ -34,19 +34,19 @@
 # define isInfF     isinf
 #else
 static VVA_OKUNUSED VVA_CONST VVA_CHECKRESULT
-inline bool isFiniteF (const float v) noexcept {
+VVA_ALWAYS_INLINE bool isFiniteF (const float v) noexcept {
   const union { float f; vuint32 x; } u = {v};
   return ((u.x&0x7f800000u) != 0x7f800000u);
 }
 
 static VVA_OKUNUSED VVA_CONST VVA_CHECKRESULT
-inline bool isNaNF (const float v) noexcept {
+VVA_ALWAYS_INLINE bool isNaNF (const float v) noexcept {
   const union { float f; vuint32 x; } u = {v};
   return ((u.x<<1) > 0xff000000u);
 }
 
 static VVA_OKUNUSED VVA_CONST VVA_CHECKRESULT
-inline bool isInfF (const float v) noexcept {
+VVA_ALWAYS_INLINE bool isInfF (const float v) noexcept {
   const union { float f; vuint32 x; } u = {v};
   return ((u.x<<1) == 0xff000000u);
 }
@@ -54,7 +54,7 @@ inline bool isInfF (const float v) noexcept {
 
 // is float denormalised?
 static VVA_OKUNUSED VVA_CONST VVA_CHECKRESULT
-bool isDenormalF (const float v) noexcept {
+VVA_ALWAYS_INLINE bool isDenormalF (const float v) noexcept {
   const union { float f; uint32_t x; } u = {v};
   return ((u.x&0x7f800000u) == 0u && (u.x&0x007fffffu) != 0u);
 }
@@ -165,7 +165,7 @@ template <class T> constexpr VVA_CONST VVA_CHECKRESULT VVA_ALWAYS_INLINE T max3 
 
 //int mlog2 (int val);
 //int mround (float);
-static VVA_OKUNUSED VVA_CHECKRESULT inline int mround (const float Val) noexcept { return (int)floorf(Val+0.5f); }
+static VVA_OKUNUSED VVA_PURE VVA_CHECKRESULT VVA_ALWAYS_INLINE int mround (const float Val) noexcept { return (int)floorf(Val+0.5f); }
 
 static VVA_OKUNUSED VVA_CONST VVA_CHECKRESULT
 inline int ToPowerOf2 (int val) noexcept {
@@ -241,7 +241,7 @@ inline double AngleMod180D (double angle) noexcept {
 }
 
 static VVA_OKUNUSED VVA_PURE VVA_CHECKRESULT
-inline float AngleDiff (float afrom, float ato) noexcept { return AngleMod(ato-afrom+180.0f)-180.0f; }
+VVA_ALWAYS_INLINE  float AngleDiff (float afrom, float ato) noexcept { return AngleMod(ato-afrom+180.0f)-180.0f; }
 
 #ifdef NO_SINCOS
 
@@ -265,45 +265,45 @@ static VVA_OKUNUSED inline void sincosd_dumb (double x, double *vsin, double *vc
 
 #endif
 
-static VVA_OKUNUSED VVA_PURE VVA_CHECKRESULT inline float msin (const float angle) noexcept { return sinf(DEG2RADF(angle)); }
-static VVA_OKUNUSED VVA_PURE VVA_CHECKRESULT inline float mcos (const float angle) noexcept { return cosf(DEG2RADF(angle)); }
-static VVA_OKUNUSED VVA_PURE VVA_CHECKRESULT inline float mtan (const float angle) noexcept { return tanf(DEG2RADF(angle)); }
-static VVA_OKUNUSED VVA_PURE VVA_CHECKRESULT inline float masin (const float x) noexcept { return RAD2DEGF(asinf(x)); }
-static VVA_OKUNUSED VVA_PURE VVA_CHECKRESULT inline float macos (const float x) noexcept { return RAD2DEGF(acosf(x)); }
-static VVA_OKUNUSED VVA_PURE VVA_CHECKRESULT inline float matan (const float y, const float x) noexcept { return RAD2DEGF(atan2f(y, x)); }
-static VVA_OKUNUSED VVA_PURE VVA_CHECKRESULT inline double matand (const double y, const double x) noexcept { return RAD2DEGD(atan2(y, x)); }
-static VVA_OKUNUSED inline void msincos (const float angle, float *vsin, float *vcos) noexcept { return sincosf(DEG2RADF(angle), vsin, vcos); }
-static VVA_OKUNUSED inline void msincosd (const double angle, double *vsin, double *vcos) noexcept { return sincosd(DEG2RADD(angle), vsin, vcos); }
+static VVA_OKUNUSED VVA_PURE VVA_CHECKRESULT VVA_ALWAYS_INLINE float msin (const float angle) noexcept { return sinf(DEG2RADF(angle)); }
+static VVA_OKUNUSED VVA_PURE VVA_CHECKRESULT VVA_ALWAYS_INLINE float mcos (const float angle) noexcept { return cosf(DEG2RADF(angle)); }
+static VVA_OKUNUSED VVA_PURE VVA_CHECKRESULT VVA_ALWAYS_INLINE float mtan (const float angle) noexcept { return tanf(DEG2RADF(angle)); }
+static VVA_OKUNUSED VVA_PURE VVA_CHECKRESULT VVA_ALWAYS_INLINE float masin (const float x) noexcept { return RAD2DEGF(asinf(x)); }
+static VVA_OKUNUSED VVA_PURE VVA_CHECKRESULT VVA_ALWAYS_INLINE float macos (const float x) noexcept { return RAD2DEGF(acosf(x)); }
+static VVA_OKUNUSED VVA_PURE VVA_CHECKRESULT VVA_ALWAYS_INLINE float matan (const float y, const float x) noexcept { return RAD2DEGF(atan2f(y, x)); }
+static VVA_OKUNUSED VVA_PURE VVA_CHECKRESULT VVA_ALWAYS_INLINE double matand (const double y, const double x) noexcept { return RAD2DEGD(atan2(y, x)); }
+static VVA_OKUNUSED VVA_ALWAYS_INLINE void msincos (const float angle, float *vsin, float *vcos) noexcept { return sincosf(DEG2RADF(angle), vsin, vcos); }
+static VVA_OKUNUSED VVA_ALWAYS_INLINE void msincosd (const double angle, double *vsin, double *vcos) noexcept { return sincosd(DEG2RADD(angle), vsin, vcos); }
 
 
-static VVA_OKUNUSED VVA_CONST VVA_CHECKRESULT inline float ByteToAngle (vuint8 angle) noexcept { return (float)(angle*360.0f/256.0f); }
-static VVA_OKUNUSED VVA_PURE VVA_CHECKRESULT inline vuint8 AngleToByte (const float angle) noexcept { return (vuint8)(AngleMod(angle)*256.0f/360.0f); }
+static VVA_OKUNUSED VVA_CONST VVA_CHECKRESULT VVA_ALWAYS_INLINE float ByteToAngle (vuint8 angle) noexcept { return (float)(angle*360.0f/256.0f); }
+static VVA_OKUNUSED VVA_PURE VVA_CHECKRESULT VVA_ALWAYS_INLINE vuint8 AngleToByte (const float angle) noexcept { return (vuint8)(AngleMod(angle)*256.0f/360.0f); }
 
 
-static inline VVA_OKUNUSED VVA_CONST VVA_CHECKRESULT
-vint32 scaleInt (vint32 a, vint32 b, vint32 c) noexcept { return (vint32)(((vint64)a*b)/c); }
+static VVA_OKUNUSED VVA_CONST VVA_CHECKRESULT
+VVA_ALWAYS_INLINE vint32 scaleInt (vint32 a, vint32 b, vint32 c) noexcept { return (vint32)(((vint64)a*b)/c); }
 
-static inline VVA_OKUNUSED VVA_CONST VVA_CHECKRESULT
-vuint32 scaleUInt (vuint32 a, vuint32 b, vuint32 c) noexcept { return (vuint32)(((vuint64)a*b)/c); }
+static VVA_OKUNUSED VVA_CONST VVA_CHECKRESULT
+VVA_ALWAYS_INLINE vuint32 scaleUInt (vuint32 a, vuint32 b, vuint32 c) noexcept { return (vuint32)(((vuint64)a*b)/c); }
 
 
 // this is actually branch-less for ints on x86, and even for longs on x86_64
 static VVA_OKUNUSED VVA_CONST VVA_CHECKRESULT
-inline vuint8 clampToByte (vint32 n) noexcept {
+VVA_ALWAYS_INLINE vuint8 clampToByte (vint32 n) noexcept {
   n &= -(vint32)(n >= 0);
   return (vuint8)(n|((255-(vint32)n)>>31));
   //return (n < 0 ? 0 : n > 255 ? 255 : n);
 }
 
 static VVA_OKUNUSED VVA_CONST VVA_CHECKRESULT
-inline vuint8 clampToByteU (vuint32 n) noexcept {
+VVA_ALWAYS_INLINE vuint8 clampToByteU (vuint32 n) noexcept {
   return (vuint8)((n&0xff)|(255-((-(vint32)(n < 256))>>24)));
 }
 
 
 // Neumaier-Kahan algorithm
 static VVA_OKUNUSED VVA_PURE VVA_CHECKRESULT
-inline float neumsum2 (float v0, const float v1) noexcept {
+VVA_ALWAYS_INLINE float neumsum2 (float v0, const float v1) noexcept {
   // one iteration
   const float t = v0+v1;
   return t+(fabsf(v0) >= fabsf(v1) ? (v0-t)+v1 : (v1-t)+v0);
@@ -311,7 +311,7 @@ inline float neumsum2 (float v0, const float v1) noexcept {
 
 // Neumaier-Kahan algorithm
 static VVA_OKUNUSED VVA_PURE VVA_CHECKRESULT
-inline float neumsum3 (float v0, const float v1, const float v2) noexcept {
+VVA_ALWAYS_INLINE float neumsum3 (float v0, const float v1, const float v2) noexcept {
   // first iteration
   const float t = v0+v1;
   const float c = (fabsf(v0) >= fabsf(v1) ? (v0-t)+v1 : (v1-t)+v0);
@@ -322,7 +322,7 @@ inline float neumsum3 (float v0, const float v1, const float v2) noexcept {
 
 // Neumaier-Kahan algorithm
 static VVA_OKUNUSED VVA_PURE VVA_CHECKRESULT
-inline float neumsum4 (float v0, const float v1, const float v2, const float v3) noexcept {
+VVA_ALWAYS_INLINE float neumsum4 (float v0, const float v1, const float v2, const float v3) noexcept {
   // first iteration
   float t = v0+v1;
   float c = (fabsf(v0) >= fabsf(v1) ? (v0-t)+v1 : (v1-t)+v0);
@@ -340,7 +340,7 @@ inline float neumsum4 (float v0, const float v1, const float v2, const float v3)
 
 // Neumaier-Kahan algorithm
 static VVA_OKUNUSED VVA_PURE VVA_CHECKRESULT
-inline double neumsum2D (double v0, const double v1) noexcept {
+VVA_ALWAYS_INLINE double neumsum2D (double v0, const double v1) noexcept {
   // one iteration
   const double t = v0+v1;
   return t+(fabs(v0) >= fabs(v1) ? (v0-t)+v1 : (v1-t)+v0);
@@ -348,7 +348,7 @@ inline double neumsum2D (double v0, const double v1) noexcept {
 
 // Neumaier-Kahan algorithm
 static VVA_OKUNUSED VVA_PURE VVA_CHECKRESULT
-inline double neumsum3D (double v0, const double v1, const double v2) noexcept {
+VVA_ALWAYS_INLINE double neumsum3D (double v0, const double v1, const double v2) noexcept {
   // first iteration
   const double t = v0+v1;
   const double c = (fabs(v0) >= fabs(v1) ? (v0-t)+v1 : (v1-t)+v0);
@@ -365,8 +365,8 @@ protected:
   float currValue; // = 0;
 
 public:
-  RunningAverageExp () noexcept : fadeoff(0.1f), currValue(0.0f) {}
-  RunningAverageExp (float aFadeoff) noexcept : fadeoff(aFadeoff), currValue(0.0f) {}
+  inline RunningAverageExp () noexcept : fadeoff(0.1f), currValue(0.0f) {}
+  inline RunningAverageExp (float aFadeoff) noexcept : fadeoff(aFadeoff), currValue(0.0f) {}
 
   inline void reset () noexcept { currValue = 0.0f; }
 
