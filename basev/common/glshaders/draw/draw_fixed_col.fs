1b321c5e8f44405fe33775990579c8f4fdd65bf5
ketmar@ketmar.no-ip.org <ketmar@ketmar.no-ip.org>
2021-08-08 14:19:25 +0000

    shaders: cosmetix
    FossilOrigin-Name: e1ec99bfbd2b9a1137fc06947df6bfc2b45b4c254d2f52aa9aa755939a160975

--- basev/common/glshaders/draw/draw_fixed_col.fs
+++ basev/common/glshaders/draw/draw_fixed_col.fs
@@ -6,10 +6,6 @@ uniform vec4 Color;
 
 void main () {
   // we got a non-premultiplied color, convert it
-  vec4 FinalColor_1;
-  FinalColor_1.r = Color.r*Color.a;
-  FinalColor_1.g = Color.g*Color.a;
-  FinalColor_1.b = Color.b*Color.a;
-  FinalColor_1.a = Color.a;
-  out_FragValue0 = FinalColor_1;
+  vec4 FinalColor = vec4(Color.rgb*Color.a, Color.a);
+  out_FragValue0 = FinalColor;
 }
