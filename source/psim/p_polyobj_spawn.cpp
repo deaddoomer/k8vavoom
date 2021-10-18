f8faa4b975fc62d084db5147a793fac260366cae
ketmar <ketmar@ketmar.no-ip.org>
2021-10-18 04:24:38 +0000

    psim: oops, i did it again; quickfix for [6afc139e07], lol! this should **reallyreally** fix [56ca276cf6] ;-)
    FossilOrigin-Name: b00aacf75ed70c3b673a4dcf0f1b71fe6aef732431d8916cbb04ec398245f7df

--- source/psim/p_polyobj_spawn.cpp
+++ source/psim/p_polyobj_spawn.cpp
@@ -615,7 +615,7 @@ void VLevel::TranslatePolyobjToStartSpot (PolyAnchorPoint_t *anchor) {
   if (po->posector) {
     // 3d polyobject
     // use inner sector Z (this won't work for slopes, but 3d pobj slopes are not supported anyway)
-    po->startSpot.z = po->posector->floor.minz-deltaZ;
+    po->startSpot.z = po->posector->floor.minz-startHeight;
 
     if (dbg_pobj_verbose_spawn.asBool()) {
       GCon->Logf(NAME_Debug, "000: pobj #%d: floor=(%g,%g,%g:%g) %g:%g; ceiling=(%g,%g,%g:%g) %g:%g  deltaZ=%g", po->tag,
