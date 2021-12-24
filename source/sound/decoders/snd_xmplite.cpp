316a6a1f481197029b1e14e1e5d07404048b02f0
ketmar <ketmar@ketmar.no-ip.org>
2021-12-24 01:36:34 +0000

    xmplite: sound: more windoze fixes
    FossilOrigin-Name: e19a606a9058aeb4248bdf13eb21b6aae7a702f7235c05282864ef990ea28f9c

--- source/sound/decoders/snd_xmplite.cpp
+++ source/sound/decoders/snd_xmplite.cpp
@@ -23,6 +23,8 @@
 //**  along with this program.  If not, see <http://www.gnu.org/licenses/>.
 //**
 //**************************************************************************
+//k8: for new xmplite
+#define BUILDING_STATIC
 #include <xmp.h>
 
 #include "../../gamedefs.h"
