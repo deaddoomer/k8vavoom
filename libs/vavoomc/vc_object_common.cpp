736f86a6a05be883fa687cd2b4fa7dfac1f22600
ketmar <ketmar@ketmar.no-ip.org>
2023-07-07 01:43:32 +0000

    updated year in copyright comments
    FossilOrigin-Name: cc1e748aef789f66284aab0401d2528b24de02447554ade251bde291e37dcdaf

--- libs/vavoomc/vc_object_common.cpp
+++ libs/vavoomc/vc_object_common.cpp
@@ -7,7 +7,7 @@
 //**       #    ##    ##    #      ####     ####   ##       ##
 //**
 //**  Copyright (C) 1999-2006 Jānis Legzdiņš
-//**  Copyright (C) 2018-2022 Ketmar Dark
+//**  Copyright (C) 2018-2023 Ketmar Dark
 //**
 //**  This program is free software: you can redistribute it and/or modify
 //**  it under the terms of the GNU General Public License as published by
